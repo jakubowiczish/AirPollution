750df6de317f2acffe5600248a15d83363a24c1e
jakubowiczish <jakub77plotnikowski@gmail.com>
2018-12-12 15:46:45 +0100

    Init Project

--- src/main/java/AirPollution/App.java
+++ src/main/java/AirPollution/App.java
@@ -4,11 +4,13 @@
 package AirPollution;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        try {
+            System.out.println(URLReader.getHTML());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
---
+++ src/main/java/AirPollution/DataStation.java
@@ -0,0 +1,9 @@
+package AirPollution;
+
+/**
+ * Class representing station, used to search by station name
+ */
+public class DataStation {
+    int id;
+    String stationName;
+}
