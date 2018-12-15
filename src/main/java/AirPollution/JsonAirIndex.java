803afbef36c9aa46881b44d677bb5c2c0715155d
jakubowiczish <jakub77plotnikowski@gmail.com>
2018-12-15 16:43:27 +0100

    Add gradle fix and AirIndex toString() method

--- src/main/java/AirPollution/JsonAirIndex.java
+++ src/main/java/AirPollution/JsonAirIndex.java
@@ -35,137 +35,34 @@ public class JsonAirIndex {
     String c6h6CalcDate;
     String c6h6SourceDataDate;
 
-
-//    public static Map<String, Object> convertJsonAirIndexToAirIndex(Map<String, Object> JsonAirIndexMap) {
-//        Map<String, Object> AirIndexMap = new HashMap<>();
-//        SimpleDateFormat usedDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//        for (Map.Entry<String, Object> entry : JsonAirIndexMap.entrySet()) {
-//            if (entry.getKey().contains("-")) {
-//                AirIndexMap.entrySet().add(entry);
-//            } else {
-//                entry.getValue();
-//                entry.setValue(null);
-//                AirIndexMap.entrySet().add(entry);
-//            }
-//        }
-//        return AirIndexMap;
-//    }
-
-
-//    public AirIndex convertJsonAirIndex() throws ParseException {
-//        SimpleDateFormat usedDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//        AirIndex newIndex = new AirIndex();
-//
-//        newIndex.stIndexLevel = this.stIndexLevel;
-//
-//        if (this.stCalcDate != null && this.stCalcDate.contains("-")) {
-//            newIndex.stCalcDate = Utils.toCalendar(usedDateFormat.parse(this.stCalcDate));
-//        } else newIndex.stCalcDate = null;
-//
-//        if (this.stSourceDataDate != null && this.stSourceDataDate.contains("-")) {
-//            newIndex.stSourceDataDate = Utils.toCalendar(usedDateFormat.parse(this.stCalcDate));
-//        } else newIndex.stSourceDataDate = null;
-//
-//        newIndex.so2IndexLevel = this.so2IndexLevel;
-//
-//        if (this.so2CalcDate != null && this.so2CalcDate.contains("-")) {
-//            newIndex.so2CalcDate = Utils.toCalendar(usedDateFormat.parse(this.so2CalcDate));
-//        } else newIndex.so2CalcDate = null;
-//
-//        if (this.so2SourceDataDate != null && this.so2SourceDataDate.contains("-")) {
-//            newIndex.so2SourceDataDate = Utils.toCalendar(usedDateFormat.parse(this.so2SourceDataDate));
-//        } else newIndex.so2SourceDataDate = null;
-//
-//
-//        newIndex.no2IndexLevel = this.no2IndexLevel;
-//
-//        if (this.no2CalcDate != null && this.no2CalcDate.contains("-")) {
-//            newIndex.no2CalcDate = Utils.toCalendar(usedDateFormat.parse(this.no2CalcDate));
-//        } else newIndex.no2CalcDate = null;
-//        if (this.no2SourceDataDate != null && this.no2SourceDataDate.contains("-")) {
-//            newIndex.no2SourceDataDate = Utils.toCalendar(usedDateFormat.parse(this.no2SourceDataDate));
-//        } else newIndex.no2SourceDataDate = null;
-//
-//
-//        newIndex.coIndexLevel = this.coIndexLevel;
-//
-//        if (this.coCalcDate != null && this.coCalcDate.contains("-")) {
-//            newIndex.coCalcDate = Utils.toCalendar(usedDateFormat.parse(this.coCalcDate));
-//        } else newIndex.coCalcDate = null;
-//        if (this.coSourceDataDate != null && this.coSourceDataDate.contains("-")) {
-//            newIndex.coSourceDataDate = Utils.toCalendar(usedDateFormat.parse(this.coSourceDataDate));
-//        } else newIndex.coSourceDataDate = null;
-//
-//
-//        newIndex.pm10IndexLevel = this.pm10IndexLevel;
-//
-//        if (this.pm10CalcDate != null && this.pm10CalcDate.contains("-")) {
-//            newIndex.pm10CalcDate = Utils.toCalendar(usedDateFormat.parse(this.pm10CalcDate));
-//        } else newIndex.pm10CalcDate = null;
-//        if (this.pm10SourceDataDate != null && this.pm10SourceDataDate.contains("-")) {
-//            newIndex.pm10SourceDataDate = Utils.toCalendar(usedDateFormat.parse(this.pm10SourceDataDate));
-//        } else newIndex.pm10SourceDataDate = null;
-//
-//
-//        newIndex.pm25IndexLevel = this.pm25IndexLevel;
-//
-//        if (this.pm25CalcDate != null && this.pm25CalcDate.contains("-")) {
-//            newIndex.pm25CalcDate = Utils.toCalendar(usedDateFormat.parse(this.pm25CalcDate));
-//        } else newIndex.pm25CalcDate = null;
-//        if (this.pm25SourceDataDate != null && this.pm25SourceDataDate.contains("-")) {
-//            newIndex.pm25SourceDataDate = Utils.toCalendar(usedDateFormat.parse(this.pm25SourceDataDate));
-//        } else newIndex.pm25SourceDataDate = null;
-//
-//        newIndex.o3IndexLevel = this.o3IndexLevel;
-//
-//        if (this.o3CalcDate != null && this.o3CalcDate.contains("-")) {
-//            newIndex.o3CalcDate = Utils.toCalendar(usedDateFormat.parse(this.o3CalcDate));
-//        } else newIndex.o3CalcDate = null;
-//        if (this.o3SourceDataDate != null && this.o3SourceDataDate.contains("-")) {
-//            newIndex.o3SourceDataDate = Utils.toCalendar(usedDateFormat.parse(this.o3SourceDataDate));
-//        } else newIndex.o3SourceDataDate = null;
-//
-//        newIndex.c6h6IndexLevel = this.c6h6IndexLevel;
-//
-//        if (this.c6h6CalcDate != null && this.c6h6CalcDate.contains("-")) {
-//            newIndex.c6h6CalcDate = Utils.toCalendar(usedDateFormat.parse(this.c6h6CalcDate));
-//        } else newIndex.c6h6CalcDate = null;
-//        if (this.c6h6SourceDataDate != null && this.c6h6SourceDataDate.contains("-")) {
-//            newIndex.c6h6SourceDataDate = Utils.toCalendar(usedDateFormat.parse(this.c6h6SourceDataDate));
-//        } else newIndex.c6h6SourceDataDate = null;
-//
-//        return newIndex;
-//    }
-
-
     @Override
     public String toString() {
-        return "JsonAirIndex{" +
-                "id=" + id +
-                ", stIndexLevel=" + stIndexLevel +
-                ", stCalcDate='" + stCalcDate + '\'' +
-                ", stSourceDataDate='" + stSourceDataDate + '\'' +
-                ", so2IndexLevel=" + so2IndexLevel +
-                ", so2CalcDate='" + so2CalcDate + '\'' +
-                ", so2SourceDataDate='" + so2SourceDataDate + '\'' +
-                ", no2IndexLevel=" + no2IndexLevel +
-                ", no2CalcDate='" + no2CalcDate + '\'' +
-                ", no2SourceDataDate='" + no2SourceDataDate + '\'' +
-                ", coIndexLevel=" + coIndexLevel +
-                ", coCalcDate='" + coCalcDate + '\'' +
-                ", coSourceDataDate='" + coSourceDataDate + '\'' +
-                ", pm10IndexLevel=" + pm10IndexLevel +
-                ", pm10CalcDate='" + pm10CalcDate + '\'' +
-                ", pm10SourceDataDate='" + pm10SourceDataDate + '\'' +
-                ", pm25IndexLevel=" + pm25IndexLevel +
-                ", pm25CalcDate='" + pm25CalcDate + '\'' +
-                ", pm25SourceDataDate='" + pm25SourceDataDate + '\'' +
-                ", o3IndexLevel=" + o3IndexLevel +
-                ", o3CalcDate='" + o3CalcDate + '\'' +
-                ", o3SourceDataDate='" + o3SourceDataDate + '\'' +
-                ", c6h6IndexLevel=" + c6h6IndexLevel +
-                ", c6h6CalcDate='" + c6h6CalcDate + '\'' +
-                ", c6h6SourceDataDate='" + c6h6SourceDataDate + '\'' +
+        return "AIR_INDEX {" + "\n" +
+                "   id:                  " + id + "\n" +
+                "   stIndexLevel:        " + stIndexLevel + "\n" +
+                "   stCalcDate:         '" + stCalcDate + '\'' + "\n" +
+                "   stSourceDataDate:   '" + stSourceDataDate + '\'' + "\n" +
+                "   so2IndexLevel:       " + so2IndexLevel + "\n" +
+                "   so2CalcDate:        '" + so2CalcDate + '\'' + "\n" +
+                "   so2SourceDataDate:  '" + so2SourceDataDate + '\'' + "\n" +
+                "   no2IndexLevel:       " + no2IndexLevel + "\n" +
+                "   no2CalcDate:        '" + no2CalcDate + '\'' + "\n" +
+                "   no2SourceDataDate:  '" + no2SourceDataDate + '\'' + "\n" +
+                "   coIndexLevel:        " + coIndexLevel + "\n" +
+                "   coCalcDate:         '" + coCalcDate + '\'' + "\n" +
+                "   coSourceDataDate:   '" + coSourceDataDate + '\'' + "\n" +
+                "   pm10IndexLevel:      " + pm10IndexLevel + "\n" +
+                "   pm10CalcDate:       '" + pm10CalcDate + '\'' + "\n" +
+                "   pm10SourceDataDate: '" + pm10SourceDataDate + '\'' + "\n" +
+                "   pm25IndexLevel:      " + pm25IndexLevel + "\n" +
+                "   pm25CalcDate:       '" + pm25CalcDate + '\'' + "\n" +
+                "   pm25SourceDataDate: '" + pm25SourceDataDate + '\'' + "\n" +
+                "   o3IndexLevel:        " + o3IndexLevel + "\n" +
+                "   o3CalcDate:         '" + o3CalcDate + '\'' + "\n" +
+                "   o3SourceDataDate:   '" + o3SourceDataDate + '\'' + "\n" +
+                "   c6h6IndexLevel:      " + c6h6IndexLevel + "\n" +
+                "   c6h6CalcDate:       '" + c6h6CalcDate + '\'' + "\n" +
+                "   c6h6SourceDataDate: '" + c6h6SourceDataDate + '\'' + "\n" +
                 '}';
     }
 
@@ -175,10 +72,10 @@ public class JsonAirIndex {
 
         @Override
         public String toString() {
-            return "IndexLevel{" +
-                    "id=" + id +
-                    ", indexLevelName='" + indexLevelName + '\'' +
-                    '}';
+            return "{" + "\n" +
+                    "       id:" + id + "\n" +
+                    "       indexLevelName:'" + indexLevelName + '\'' + "\n" +
+                    "   }";
         }
     }
 }
