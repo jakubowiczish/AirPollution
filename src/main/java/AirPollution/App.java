27061611817cd11d66c6f9198ed8caf9b877445b
jakubowiczish <jakub77plotnikowski@gmail.com>
2018-12-16 00:49:45 +0100

    Add additional names for Options when running app through command line

--- src/main/java/AirPollution/App.java
+++ src/main/java/AirPollution/App.java
@@ -13,19 +13,19 @@ import picocli.CommandLine.Option;
         )
 public class App implements Runnable {
 
-    @Option(names = {"--stationName"}, description = "Station name for Air Index")
+    @Option(names = {"-s", "--stationName"}, description = "Station name for Air Index")
     private String stationName;
 
-    @Option(names = {"--date"}, description = "Date of measurement")
+    @Option(names = {"-d", "--date"}, description = "Date of measurement")
     private String date;
 
-    @Option(names = {"--parameterName"}, description = "Name of the parameter")
+    @Option(names = {"-par", "--parameterName"}, description = "Name of the parameter")
     private String parameterName;
 
-    @Option(names = {"--startDate"}, description = "Start date")
+    @Option(names = {"-st", "--startDate"}, description = "Start date")
     private String startDate;
 
-    @Option(names = {"--endDate"}, description = "End date")
+    @Option(names = {"-end", "--endDate"}, description = "End date")
     private String endDate;
 
     public static void main(String[] args) {
@@ -48,10 +48,11 @@ public class App implements Runnable {
 //            System.out.println(optionsHandler.currentParameterValue(date, stationName, parameterName));
 //        }
 //        java -jar AirPollution-1.0-all.jar --startDate="2018-12-15 19:00:00" --endDate="2018-12-15 21:00:00" --parameterName="O3"
-//        if (startDate != null && endDate != null && parameterName != null) {
+//        if (startDate != null && endDate !=java -jar AirPollution-1.0-all.jar -st "2018-12-15 19:00:00" -end "2018-12-15 21:00:00" -par "O3" -s "Tarnów, ul. Bitwy pod Studziankami" null && parameterName != null) {
 //            System.out.println(optionsHandler.averagePollutionValue(startDate, endDate, parameterName));
 //        }
 //        java -jar AirPollution-1.0-all.jar --startDate="2018-12-15 19:00:00" --endDate="2018-12-15 21:00:00" --parameterName="O3" --stationName="Tarnów, ul. Bitwy pod Studziankami"
+//        java -jar AirPollution-1.0-all.jar -st "2018-12-15 19:00:00" -end "2018-12-15 21:00:00" -par "O3" -s "Tarnów, ul. Bitwy pod Studziankami"
         if(startDate != null && endDate != null && parameterName != null && stationName != null) {
             System.out.println(optionsHandler.averagePollutionValueForConcreteStation(startDate, endDate, parameterName, stationName));
         }
